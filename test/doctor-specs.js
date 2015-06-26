eea593aefad3f4dccb6b0da800bf344006c36117
sebv <seb.vincent@gmail.com>
2015-06-26 19:58:57 +0800

    added ios checks

--- test/doctor-specs.js
+++ test/doctor-specs.js
@@ -1,6 +1,6 @@
 // transpile:mocha
 
-import {Doctor, Issue} from '../lib/doctor';
+import {Doctor, DoctorCheck} from '../lib/doctor';
 import chai from 'chai';
 import sinon from 'sinon';
 import 'mochawait';
@@ -19,38 +19,38 @@ describe('doctor', () => {
 
   it('register', () => {
     let doctor = new Doctor();
-    doctor.issues.should.have.length(0);
-    doctor.register(new Issue());
-    doctor.issues.should.have.length(1);
-    doctor.register([new Issue(), new Issue()]);
-    doctor.issues.should.have.length(3);
+    doctor.checks.should.have.length(0);
+    doctor.register(new DoctorCheck());
+    doctor.checks.should.have.length(1);
+    doctor.register([new DoctorCheck(), new DoctorCheck()]);
+    doctor.checks.should.have.length(3);
   });
 
   function configure() {
     let doctor = new Doctor();
-    let issues = [new Issue(), new Issue(), new Issue()];
-    doctor.register(issues);
-    return {doctor, issues};
+    let checks = [new DoctorCheck(), new DoctorCheck(), new DoctorCheck()];
+    doctor.register(checks);
+    return {doctor, checks};
   }
 
   it('diagnose',async () => {
-    let {doctor, issues} = configure();
-    let mockedIssues = _.map(issues, (issue) => { return sandbox.mock(issue); });
-    mockedIssues[0].expects('diagnose').once().returns({exists: false, message: "All Good!"});
-    mockedIssues[1].expects('diagnose').once().returns({exists: true, message: "Oh No!"});
-    mockedIssues[2].expects('diagnose').once().returns({exists: true, message: "Oh No!"});
+    let {doctor, checks} = configure();
+    let mockedDoctorChecks = _.map(checks, (check) => { return sandbox.mock(check); });
+    mockedDoctorChecks[0].expects('diagnose').once().returns({ok: true, message: "All Good!"});
+    mockedDoctorChecks[1].expects('diagnose').once().returns({ok: false, message: "Oh No!"});
+    mockedDoctorChecks[2].expects('diagnose').once().returns({ok: false, message: "Oh No!"});
     await doctor.diagnose();
-    for(let mock of mockedIssues) { mock.verify(); }
+    for(let mock of mockedDoctorChecks) { mock.verify(); }
     doctor.toFix.should.have.length(2);
   });
 
   it('fix', async () => {
-    let {doctor, issues} = configure();
-    doctor.toFix = [issues[1], issues[2]];
-    let mockedIssues = _.map(issues, (issue) => { return sandbox.mock(issue); });
-    mockedIssues[1].expects('fix').once().returns(true);
-    mockedIssues[2].expects('fix').once().returns(true);
+    let {doctor, checks} = configure();
+    doctor.toFix = [checks[1], checks[2]];
+    let mockedDoctorChecks = _.map(checks, (check) => { return sandbox.mock(check); });
+    mockedDoctorChecks[1].expects('fix').once().returns(true);
+    mockedDoctorChecks[2].expects('fix').once().returns(true);
     await doctor.fix();
-    for(let mock of mockedIssues) { mock.verify(); }
+    for(let mock of mockedDoctorChecks) { mock.verify(); }
   });
 });
