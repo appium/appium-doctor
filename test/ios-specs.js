4779b15554cd9c2bdbc28abde3ae760eba934dd9
sebv <seb.vincent@gmail.com>
2015-07-01 20:48:15 +0800

    increasing coverage

--- test/ios-specs.js
+++ test/ios-specs.js
@@ -1,21 +1,193 @@
 // transpile:mocha
 
-import {authorizeIos} from '../lib/ios';
-import {cp} from '../lib/utils';
+import {authorizeIos, XcodeCheck, XcodeCmdLineToolsCheck,
+  DevToolsSecurityCheck, AuthorizationDbCheck, NodeBinaryCheck } from '../lib/ios';
+import {cp, fs} from '../lib/utils';
+import * as utils from '../lib/utils';
+import NodeDetector from '../lib/node-detector';
 import chai from 'chai';
 import 'mochawait';
 import B from 'bluebird';
 import {withMocks, verifyAll} from './mock-utils';
 
 chai.should();
+let P = Promise;
 
 describe('ios', () => {
   describe('authorizeIos', withMocks({cp}, (mocks) => {
     it('should work',async () => {
-      mocks.cp.expects('exec').once().returns(B.resolve(["", ""]));
+      mocks.cp.expects('exec').once().returns(P.resolve(["", ""]));
       await authorizeIos();
       verifyAll(mocks);
     });
   }));
 
+  describe('XcodeCheck', withMocks({cp, fs} ,(mocks) => {
+    let check = new XcodeCheck();
+    it('diagnose - success', async () => {
+      mocks.cp.expects('exec').once().returns(P.resolve(['/a/b/c/d\n', '']));
+      mocks.fs.expects('exists').once().returns(P.resolve(true));
+      (await check.diagnose()).should.deep.equal({
+        ok: true,
+        message: 'Xcode is installed at /a/b/c/d.'
+      });
+      verifyAll(mocks);
+    });
+    it('diagnose - failure - xcode-select', async () => {
+      mocks.cp.expects('exec').once().returns(P.reject(new Error('Something wrong!')));
+      (await check.diagnose()).should.deep.equal({
+        ok: false,
+        message: 'Xcode is NOT installed!'
+      });
+      verifyAll(mocks);
+    });
+    it('diagnose - failure - path not exists', async () => {
+      mocks.cp.expects('exec').once().returns(
+        P.resolve(['/a/b/c/d\n', '']));
+      mocks.fs.expects('exists').once().returns(P.resolve(false));
+      (await check.diagnose()).should.deep.equal({
+        ok: false,
+        message: 'Xcode cannot be found at /a/b/c/d!'
+      });
+      verifyAll(mocks);
+    });
+    it('fix', async () => {
+      mocks.cp.expects('exec').once().returns(
+        P.resolve(['', '']));
+      await check.fix();
+      verifyAll(mocks);
+    });
+  }));
+  describe('XcodeCmdLineToolsCheck', withMocks({cp, utils} ,(mocks) => {
+    let check = new XcodeCmdLineToolsCheck();
+    it('diagnose - success', async () => {
+      mocks.utils.expects('macOsxVersion').once().returns(P.resolve('10.10'));
+      mocks.cp.expects('exec').once().returns(P.resolve(['1234 install-time\n', '']));
+      (await check.diagnose()).should.deep.equal({
+        ok: true,
+        message: 'Xcode Command Line Tools are installed.'
+      });
+      verifyAll(mocks);
+    });
+    it('diagnose - failure - pkgutil crash', async () => {
+      mocks.utils.expects('macOsxVersion').once().returns(B.resolve('10.10'));
+      mocks.cp.expects('exec').once().returns(Promise.reject(new Error('Something wrong!')));
+      (await check.diagnose()).should.deep.equal({
+        ok: false,
+        message: 'Xcode Command Line Tools are NOT installed!'
+      });
+      verifyAll(mocks);
+    });
+    it('diagnose - failure - no install time', async () => {
+      mocks.utils.expects('macOsxVersion').once().returns(B.resolve('10.10'));
+      mocks.cp.expects('exec').once().returns(P.resolve(['1234 abcd\n', '']));
+      (await check.diagnose()).should.deep.equal({
+        ok: false,
+        message: 'Xcode Command Line Tools are NOT installed!'
+      });
+      verifyAll(mocks);
+    });
+    it('fix', async () => {
+      mocks.cp.expects('exec').once().returns(P.resolve(['', '']));
+      await check.fix();
+      verifyAll(mocks);
+    });
+  }));
+  describe('DevToolsSecurityCheck', withMocks({cp, utils} ,(mocks) => {
+    let check = new DevToolsSecurityCheck();
+    it('diagnose - success', async () => {
+      mocks.cp.expects('exec').once().returns(P.resolve(['1234 enabled\n', '']));
+      (await check.diagnose()).should.deep.equal({
+        ok: true,
+        message: 'DevToolsSecurity is enabled.'
+      });
+      verifyAll(mocks);
+    });
+    it('diagnose - failure - DevToolsSecurity crash', async () => {
+      mocks.cp.expects('exec').once().returns(Promise.reject(new Error('Something wrong!')));
+      (await check.diagnose()).should.deep.equal({
+        ok: false,
+        message: 'DevToolsSecurity is NOT enabled!'
+      });
+      verifyAll(mocks);
+    });
+    it('diagnose - failure - not enabled', async () => {
+      mocks.cp.expects('exec').once().returns(P.resolve(['1234 abcd\n', '']));
+      (await check.diagnose()).should.deep.equal({
+        ok: false,
+        message: 'DevToolsSecurity is NOT enabled!'
+      });
+      verifyAll(mocks);
+    });
+    it('fix', async () => {
+      mocks.cp.expects('exec').once().returns(P.resolve(['', '']));
+      await check.fix();
+      verifyAll(mocks);
+    });
+  }));
+  describe('AuthorizationDbCheck', withMocks({cp, fs, utils} ,(mocks) => {
+    let check = new AuthorizationDbCheck();
+    it('diagnose - success - 10.10', async () => {
+      mocks.cp.expects('exec').once().returns(P.resolve(['1234 is-developer\n', '']));
+      (await check.diagnose()).should.deep.equal({
+        ok: true,
+        message: 'The Authorization DB is set up properly.'
+      });
+      verifyAll(mocks);
+    });
+    it('diagnose - success - 10.8', async () => {
+      mocks.cp.expects('exec').once().returns(P.reject(new Error('Oh No!')));
+      mocks.utils.expects('macOsxVersion').once().returns(P.resolve('10.8'));
+      mocks.fs.expects('readFile').once().returns(P.resolve(
+        '<key>system.privilege.taskport</key> \n <dict>\n <key>allow-root</key>\n <true/>')); 
+      (await check.diagnose()).should.deep.equal({
+        ok: true,
+        message: 'The Authorization DB is set up properly.'
+      });
+      verifyAll(mocks);
+    });
+    it('diagnose - failure - 10.10 - security', async () => {
+      mocks.cp.expects('exec').once().returns(P.reject(new Error('Oh No!')));
+      mocks.utils.expects('macOsxVersion').once().returns(P.resolve('10.10'));
+      (await check.diagnose()).should.deep.equal({
+        ok: false,
+        message: 'The Authorization DB is NOT set up properly.'
+      });
+      verifyAll(mocks);
+    });
+    it('diagnose - failure - /etc/authorization', async () => {
+      mocks.cp.expects('exec').once().returns(P.reject(new Error('Oh No!')));
+      mocks.utils.expects('macOsxVersion').once().returns(P.resolve('10.8'));
+      mocks.fs.expects('readFile').once().returns(P.resolve(''));
+      (await check.diagnose()).should.deep.equal({
+        ok: false,
+        message: 'The Authorization DB is NOT set up properly.'
+      });
+      verifyAll(mocks);
+    });
+    it('fix', async () => {
+      mocks.cp.expects('exec').once().returns(P.resolve(['', '']));
+      await check.fix();
+      verifyAll(mocks);
+    });
+  }));
+  describe('NodeBinaryCheck', withMocks({NodeDetector} ,(mocks) => {
+    let check = new NodeBinaryCheck();
+    it('diagnose - success', async () => {
+      mocks.NodeDetector.expects('detect').once().returns(P.resolve('/a/b/c/d'));
+      (await check.diagnose()).should.deep.equal({
+        ok: true,
+        message: 'The Node.js binary was found at: /a/b/c/d.'
+      });
+      verifyAll(mocks);
+    });
+    it('diagnose - failure', async () => {
+      mocks.NodeDetector.expects('detect').once().returns(P.resolve(null));
+      (await check.diagnose()).should.deep.equal({
+        ok: false,
+        message: 'The Node.js binary was NOT found!'
+      });
+      verifyAll(mocks);
+    });
+  }));
 });
--- test/node-detector-specs.js
+++ test/node-detector-specs.js
@@ -14,16 +14,14 @@ describe('NodeDetector', withMocks({fs, cp}, (mocks) => {
 
  it('retrieveInCommonPlaces - success',async () => {
     mocks.fs.expects('exists').once().returns(B.resolve(true));
-    let detector = new NodeDetector();
-    (await detector.retrieveInCommonPlaces())
+    (await NodeDetector.retrieveInCommonPlaces())
       .should.equal('/usr/local/bin/node');
     verifyAll(mocks);
   });
 
   it('retrieveInCommonPlaces - failure',async () => {
     mocks.fs.expects('exists').twice().returns(B.resolve(false));
-    let detector = new NodeDetector();
-    expect(await detector.retrieveInCommonPlaces()).to.be.a('null');
+    expect(await NodeDetector.retrieveInCommonPlaces()).to.be.a('null');
     verifyAll(mocks);
   });
 
@@ -32,23 +30,20 @@ describe('NodeDetector', withMocks({fs, cp}, (mocks) => {
     it(method + ' - success', async () => {
       mocks.cp.expects('exec').once().returns(B.resolve(['/a/b/c/d\n', '']));
       mocks.fs.expects('exists').once().returns(B.resolve(true));
-      let detector = new NodeDetector();
-      (await detector[method]())
+      (await NodeDetector[method]())
         .should.equal('/a/b/c/d');
       verifyAll(mocks);
     });
 
     it(method + ' - failure - path not found ', async () => {
       mocks.cp.expects('exec').once().returns(B.resolve(['aaa not found\n', '']));
-      let detector = new NodeDetector();
-      expect(await detector[method]()).to.be.a('null');
+      expect(await NodeDetector[method]()).to.be.a('null');
       verifyAll(mocks);
     });
     it(method + ' - failure - path not exist', async () => {
       mocks.cp.expects('exec').once().returns(B.resolve(['/a/b/c/d\n', '']));
       mocks.fs.expects('exists').once().returns(B.resolve(false));
-      let detector = new NodeDetector();
-      expect(await detector[method]()).to.be.a('null');
+      expect(await NodeDetector[method]()).to.be.a('null');
     });
   };
 
@@ -59,8 +54,7 @@ describe('NodeDetector', withMocks({fs, cp}, (mocks) => {
     mocks.fs.expects('exists').twice().returns(B.resolve(true));
     mocks.fs.expects('readFile').once().returns(
       B.resolve('{"node_bin": "/a/b/c/d"}'));
-    let detector = new NodeDetector();
-    (await detector.retrieveUsingAppiumConfigFile())
+    (await NodeDetector.retrieveUsingAppiumConfigFile())
       .should.equal('/a/b/c/d');
     verifyAll(mocks);
   });
@@ -69,8 +63,7 @@ describe('NodeDetector', withMocks({fs, cp}, (mocks) => {
     mocks.fs.expects('exists').once().returns(B.resolve(true));
     mocks.fs.expects('readFile').once().returns(
       B.resolve('{node_bin: "/a/b/c/d"}'));
-    let detector = new NodeDetector();
-    expect(await detector.retrieveUsingAppiumConfigFile())
+    expect(await NodeDetector.retrieveUsingAppiumConfigFile())
       .to.be.a('null');
     verifyAll(mocks);
   });
@@ -80,31 +73,28 @@ describe('NodeDetector', withMocks({fs, cp}, (mocks) => {
     mocks.fs.expects('exists').once().returns(B.resolve(false));
     mocks.fs.expects('readFile').once().returns(
       B.resolve('{"node_bin": "/a/b/c/d"}'));
-    let detector = new NodeDetector();
-    expect(await detector.retrieveUsingAppiumConfigFile())
+    expect(await NodeDetector.retrieveUsingAppiumConfigFile())
       .to.be.a('null');
     verifyAll(mocks);
   });
 
   it('checkForNodeBinary - success', async () => {
-    let detector = new NodeDetector();
-    mocks.detector = getSandbox(mocks).mock(detector);
-    mocks.detector.expects('retrieveInCommonPlaces').once().returns(null);
-    mocks.detector.expects('retrieveUsingWhichCommand').once().returns(null);
-    mocks.detector.expects('retrieveUsingAppleScript').returns('/a/b/c/d');
-    mocks.detector.expects('retrieveUsingAppiumConfigFile').never();
-    (await detector.detect()).should.equal('/a/b/c/d');
+    mocks.NodeDetector = getSandbox(mocks).mock(NodeDetector);
+    mocks.NodeDetector.expects('retrieveInCommonPlaces').once().returns(null);
+    mocks.NodeDetector.expects('retrieveUsingWhichCommand').once().returns(null);
+    mocks.NodeDetector.expects('retrieveUsingAppleScript').returns('/a/b/c/d');
+    mocks.NodeDetector.expects('retrieveUsingAppiumConfigFile').never();
+    (await NodeDetector.detect()).should.equal('/a/b/c/d');
     verifyAll(mocks);
   });
 
   it('checkForNodeBinary - failure', async () => {
-    let detector = new NodeDetector();
-    mocks.detector = getSandbox(mocks).mock(detector);
-    mocks.detector.expects('retrieveInCommonPlaces').once().returns(null);
-    mocks.detector.expects('retrieveUsingWhichCommand').once().returns(null);
-    mocks.detector.expects('retrieveUsingAppleScript').once().returns(null);
-    mocks.detector.expects('retrieveUsingAppiumConfigFile').once().returns(null);
-    expect(await detector.detect()).to.be.a('null');
+    mocks.NodeDetector = getSandbox(mocks).mock(NodeDetector);
+    mocks.NodeDetector.expects('retrieveInCommonPlaces').once().returns(null);
+    mocks.NodeDetector.expects('retrieveUsingWhichCommand').once().returns(null);
+    mocks.NodeDetector.expects('retrieveUsingAppleScript').once().returns(null);
+    mocks.NodeDetector.expects('retrieveUsingAppiumConfigFile').once().returns(null);
+    expect(await NodeDetector.detect()).to.be.a('null');
     verifyAll(mocks);
   });
 
