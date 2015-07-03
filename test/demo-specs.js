0568cf07e77f481f03c76e4eb3cdf3e6ee900f78
sebv <seb.vincent@gmail.com>
2015-07-03 20:18:05 +0800

    adds missing demo files

---
+++ test/demo-specs.js
@@ -0,0 +1,16 @@
+// transpile:mocha
+
+import '../lib/demo';
+import chai from 'chai';
+import 'mochawait';
+
+chai.should();
+
+describe('demo', () => {
+
+  describe('DirCheck', () => {
+    it('should work', () => {
+      // TODO
+    });
+  });
+});
