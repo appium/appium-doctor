7265e6694bd311f1de64a5f04287f54a37c12c73
sebv <seb.vincent@gmail.com>
2015-07-03 22:14:34 +0800

    prompt coverage

--- test/prompt-specs.js
+++ test/prompt-specs.js
@@ -2,16 +2,42 @@
 
 import chai from 'chai';
 import 'mochawait';
-import '../lib/prompt';
-//import B from 'bluebird';
-//import { withMocks, verifyAll } from './mock-utils';
+import {fixIt, clear } from '../lib/prompt';
+import { inquirer } from '../lib/utils';
+import { withMocks, verifyAll } from './mock-utils';
 
 chai.should();
+let P = Promise;
 
-describe('prompt', () => {
+describe('prompt', withMocks({inquirer}, (mocks) => {
 
-  it('should work',() => {
-    // TODO
+  it('fixit - yes', async () => {
+    clear();
+    mocks.inquirer.expects('prompt').once().returns(P.resolve(
+      { confirmation: 'yes' }));
+    (await fixIt()).should.equal('yes');
+    verifyAll(mocks);
   });
 
-});
+  it('fixit always ', async () => {
+    clear();
+    mocks.inquirer.expects('prompt').once().returns(P.resolve(
+      { confirmation: 'always' }));
+    (await fixIt()).should.equal('yes');
+    (await fixIt()).should.equal('yes');
+    (await fixIt()).should.equal('yes');
+    verifyAll(mocks);
+  });
+
+  it('fixit never ', async () => {
+    clear();
+    mocks.inquirer.expects('prompt').once().returns(P.resolve(
+      { confirmation: 'never' }));
+    (await fixIt()).should.equal('no');
+    (await fixIt()).should.equal('no');
+    (await fixIt()).should.equal('no');
+    verifyAll(mocks);
+  });
+
+
+}));
