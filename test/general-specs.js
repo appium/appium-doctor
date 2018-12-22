c24be968b26a53f636539b04bf4043036eabf162
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-12-22 22:10:15 +0900

    add version

--- test/general-specs.js
+++ test/general-specs.js
@@ -117,11 +117,16 @@ describe('general', function () {
       check.autofix.should.not.be.ok;
     });
     it('diagnose - success', async function () {
+      mocks.tp.expects('exec').once().returns({stdout: `ffmpeg version 4.1 Copyright (c) 2000-2018 the FFmpeg developers
+      built with Apple LLVM version 10.0.0 (clang-1000.11.45.5)
+      configuration: --prefix=/usr/local/Cellar/ffmpeg/4.1_1 --enable-shared --enable-pthreads --enable-version3 --enable-hardcoded-tables --enable-avresample --cc=clang --host-cflags= --host-ldflags= --enable-ffplay --enable-gpl --enable-libmp3lame --enable-libopus --enable-libsnappy --enable-libtheora --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libx265 --enable-libxvid --enable-lzma --enable-opencl --enable-videotoolbox
+      libavutil      56. 22.100 / 56. 22.100
+      libpostproc    55.  3.100 / 55.  3.100`, stderr: ''});
       mocks.utils.expects('resolveExecutablePath').once().returns('path/to/ffmpeg');
       (await check.diagnose()).should.deep.equal({
         ok: true,
         optional: true,
-        message: 'ffmpeg is installed at: path/to/ffmpeg'
+        message: 'ffmpeg is installed at: path/to/ffmpeg. ffmpeg version 4.1 Copyright (c) 2000-2018 the FFmpeg developers'
       });
       mocks.verify();
     });
