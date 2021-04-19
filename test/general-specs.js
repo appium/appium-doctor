c78ad2f71e3b06739d71946641eb411980877b17
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2021-04-19 00:16:55 -0700

    fix: call requirePackage in doctor

--- test/general-specs.js
+++ test/general-specs.js
@@ -8,6 +8,7 @@ import NodeDetector from '../lib/node-detector';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import { withMocks } from 'appium-test-support';
+import { node } from 'appium-support';
 import B from 'bluebird';
 import { removeColors } from './helper';
 
@@ -83,13 +84,14 @@ describe('general', function () {
     });
   }));
 
-  describe('OptionalOpencv4nodejsCommandCheck', withMocks({tp}, (mocks) => {
+  describe('OptionalOpencv4nodejsCommandCheck', withMocks({node, tp}, (mocks) => {
     let check = new OptionalOpencv4nodejsCommandCheck();
     it('autofix', function () {
       check.autofix.should.not.be.ok;
     });
     it('diagnose - success', async function () {
-      mocks.tp.expects('exec').once().returns({stdout: `
+      mocks.node.expects('requirePackage').once().returns();
+      mocks.tp.expects('exec').returns({stdout: `
         {
           "dependencies": {
             "opencv4nodejs": {
@@ -109,7 +111,8 @@ describe('general', function () {
       mocks.verify();
     });
     it('diagnose - failure', async function () {
-      mocks.tp.expects('exec').once().returns({stdout: '{}', stderr: ''});
+      mocks.node.expects('requirePackage').once().returns();
+      mocks.tp.expects('exec').returns({stdout: '{}', stderr: ''});
       (await check.diagnose()).should.deep.equal({
         ok: false,
         optional: true,
@@ -156,33 +159,22 @@ describe('general', function () {
     });
   }));
 
-  describe('OptionalMjpegConsumerCommandCheck', withMocks({tp}, (mocks) => {
+  describe('OptionalMjpegConsumerCommandCheck', withMocks({node}, (mocks) => {
     let check = new OptionalMjpegConsumerCommandCheck();
     it('autofix', function () {
       check.autofix.should.not.be.ok;
     });
     it('diagnose - success', async function () {
-      mocks.tp.expects('exec').once().returns({stdout: `
-        {
-          "dependencies": {
-            "mjpeg-consumer": {
-              "version": "1.1.0",
-              "from": "mjpeg-consumer",
-              "resolved": "https://registry.npmjs.org/mjpeg-consumer/-/mjpeg-consumer-1.1.0.tgz"
-            }
-          },
-          "path": "/path/to/node/node/v11.4.0/lib"
-        }
-      `, stderr: ''});
+      mocks.node.expects('requirePackage').once().returns();
       (await check.diagnose()).should.deep.equal({
         ok: true,
         optional: true,
-        message: 'mjpeg-consumer is installed at: /path/to/node/node/v11.4.0/lib. Installed version is: 1.1.0'
+        message: 'mjpeg-consumer is installed.'
       });
       mocks.verify();
     });
     it('diagnose - failure', async function () {
-      mocks.tp.expects('exec').once().returns({stdout: '{}', stderr: ''});
+      mocks.node.expects('requirePackage').once().throws();
       (await check.diagnose()).should.deep.equal({
         ok: false,
         optional: true,
